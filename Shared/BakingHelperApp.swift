8000bf88d10a8447530a8fc9f887a1d97b5eb9f1
NIclas Jeppsson <niclasjeppsson@gmail.com>
2022-02-07 20:40:39 +0000

    Added initial project

---
+++ Shared/BakingHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakingHelperApp.swift
+//  Shared
+//
+//  Created by Niclas Jeppsson on 22/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BakingHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
